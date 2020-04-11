e539453e1026143c54e62db9ffb8b99f8c251105
Qendrim Vllasa <qendrimvllasa@hotmail.de>
2020-04-11 14:50:24 +0200

    implement userinterface

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,32 +3,73 @@
 namespace App\DataFixtures;
 
 use App\Entity\BlogPost;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+
+    }
+
     public function load(ObjectManager $manager)
     {
-       $blogPost = new BlogPost();
-       $blogPost->setTitle('A first post!');
-       $blogPost->setPublished(new \DateTime('2018-07-01 12:00:00'));
-       $blogPost->setContent('Post text!');
-       $blogPost->setAuthor('Qendrim Vllasa');
-       $blogPost->setSlug('a-first-post');
+        $this->loadUsers($manager);
+        $this->loadBlogPosts($manager);
+    }
 
-       $manager->persist($blogPost);
+    public function loadBlogPosts(ObjectManager $manager)
+    {
+        $user = $this->getReference('user_admin') ;
 
-       $blogPost = new BlogPost();
-       $blogPost->setTitle('A second post!');
+        $blogPost = new BlogPost();
+        $blogPost->setTitle('A first post!');
+        $blogPost->setPublished(new \DateTime('2018-07-01 12:00:00'));
+        $blogPost->setContent('Post text!');
+        $blogPost->setAuthor($user);
+        $blogPost->setSlug('a-first-post');
+
+        $manager->persist($blogPost);
+
+        $blogPost = new BlogPost();
+        $blogPost->setTitle('A second post!');
 
         $blogPost->setPublished(new \DateTime('2018-07-01 12:00:00'));
         $blogPost->setContent('Post text!');
-        $blogPost->setAuthor('Qendrim Vllasa');
+        $blogPost->setAuthor($user);
         $blogPost->setSlug('a-second-post');
 
         $manager->persist($blogPost);
 
+        $manager->flush();
+    }
+
+    public function loadComments(ObjectManager $manager)
+    {
+
+    }
+
+    public function loadUsers(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setEmail('admin@admin.de');
+        $user->setName('Qendrim Vllasa');
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'test123'
+        ));
+
+        $this->addReference('user_admin', $user);
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
