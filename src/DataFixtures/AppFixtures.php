117beab3ba639fca4dd97e35906e98cfe19e5a6b
Qendrim Vllasa <qendrimvllasa@hotmail.de>
2020-04-11 13:26:57 +0200

    install easyadmin and add fake data to blogpost

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\BlogPost;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,8 +10,24 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+       $blogPost = new BlogPost();
+       $blogPost->setTitle('A first post!');
+       $blogPost->setPublished(new \DateTime('2018-07-01 12:00:00'));
+       $blogPost->setContent('Post text!');
+       $blogPost->setAuthor('Qendrim Vllasa');
+       $blogPost->setSlug('a-first-post');
+
+       $manager->persist($blogPost);
+
+       $blogPost = new BlogPost();
+       $blogPost->setTitle('A second post!');
+
+        $blogPost->setPublished(new \DateTime('2018-07-01 12:00:00'));
+        $blogPost->setContent('Post text!');
+        $blogPost->setAuthor('Qendrim Vllasa');
+        $blogPost->setSlug('a-second-post');
+
+        $manager->persist($blogPost);
 
         $manager->flush();
     }
